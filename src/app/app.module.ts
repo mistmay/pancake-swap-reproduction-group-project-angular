0949c2948b422859f595a52584fbbe2478176725
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-04-19 14:40:14 +0200

    add first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { FooterMobileComponent } from './core/footer-mobile/footer-mobile.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
+import { HeroComponent } from './views/homepage/components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    FooterMobileComponent,
+    HomepageComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
