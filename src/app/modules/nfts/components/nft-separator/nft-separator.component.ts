b69085ad79490339751bec860825c8713a45114e
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-05-16 11:50:27 +0200

    sezione nft, push post merge per perdita dati

---
+++ src/app/modules/nfts/components/nft-separator/nft-separator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-separator',
+  templateUrl: './nft-separator.component.html',
+  styleUrls: ['./nft-separator.component.scss']
+})
+export class NftSeparatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
