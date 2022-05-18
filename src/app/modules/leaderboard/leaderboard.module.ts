461b86c57030839ee727ae56388cfca8c2774056
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-05-18 16:19:07 +0200

    feat: aggiunta card teams, da sistemare

--- src/app/modules/leaderboard/leaderboard.module.ts
+++ src/app/modules/leaderboard/leaderboard.module.ts
@@ -3,16 +3,17 @@ import { LeaderboardComponent } from './views/leaderboard/leaderboard.component'
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { LeaderboardRoutingModule } from './leaderboard-routing.module';
+import { TeamsCardComponent } from './components/teams-card/teams-card.component';
 
 @NgModule({
     declarations: [
         LeaderboardComponent,
+        TeamsCardComponent
     ],
     imports: [
         SharedModule,
         CommonModule,
-        LeaderboardRoutingModule
-        
+        LeaderboardRoutingModule,
     ],
     exports: [
 
