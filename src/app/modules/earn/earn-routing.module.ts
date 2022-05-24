669715cefada1723eae7efb72a780e11da43b715
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-05-24 16:19:11 +0200

    feat: creata pagina farms e inserito al suo interno parte del componente farms-table, di cui manca il dettaglio al click; aggiunte anche le nuvolette all'hover sugli svg

--- src/app/modules/earn/earn-routing.module.ts
+++ src/app/modules/earn/earn-routing.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EarnTopComponent } from './views/earn-top.component';
+import { FarmsComponent } from './views/farms/farms.component';
 import { PoolsComponent } from './views/pools/pools.component';
 
 const routes: Routes = [
@@ -8,13 +9,17 @@ const routes: Routes = [
         path: '',
         component: EarnTopComponent,
         children: [
-                {
+            {
                 path: 'pools',
                 component: PoolsComponent
             },
             {
+                path: 'farms',
+                component: FarmsComponent
+            },
+            {
                 path: '',
-                redirectTo: "pools"
+                redirectTo: "farms"
             }
         ]
     }
