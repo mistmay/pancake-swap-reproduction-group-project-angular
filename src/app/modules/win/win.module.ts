764e7f99e4572d8812337ad9b92daa4e2bf94189
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-05-26 20:54:11 +0200

    Add: Lottery Hero

--- src/app/modules/win/win.module.ts
+++ src/app/modules/win/win.module.ts
@@ -16,6 +16,7 @@ import { RulesComponent } from './components/rules/rules.component';
 import { CompetitionFooterComponent } from './components/competition-footer/competition-footer.component';
 import { LotteryComponent } from './views/lottery/lottery.component';
 import { PredictionComponent } from './views/prediction/prediction.component';
+import { LotteryHeroComponent } from './components/lottery-hero/lottery-hero.component';
 
 @NgModule({
     declarations: [
@@ -32,7 +33,8 @@ import { PredictionComponent } from './views/prediction/prediction.component';
         RulesComponent,
         CompetitionFooterComponent,
         LotteryComponent,
-        PredictionComponent
+        PredictionComponent,
+        LotteryHeroComponent
     ],
     imports: [
         CommonModule,
