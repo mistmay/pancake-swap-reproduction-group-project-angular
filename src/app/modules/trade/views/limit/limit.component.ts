194d4d76c372bac1db96df547bf555c22db30325
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-06-03 14:36:19 +0200

    Little Fix Trade Title

--- src/app/modules/trade/views/limit/limit.component.ts
+++ src/app/modules/trade/views/limit/limit.component.ts
@@ -11,7 +11,7 @@ export class LimitComponent implements OnInit {
   constructor(private titleService: TitleService) { }
 
   ngOnInit(): void {
-    this.titleService.setTitle('Limit');
+    this.titleService.setTitle('Your Wallet');
   }
 
 }
